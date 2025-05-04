994261598ed6590786a4911bf79a574b9c9c2b33
Docz <mister-dan-zubkov@mail.ru>
2025-05-04 14:57:12 +0500

    Update dz.sh

--- dz.sh
+++ dz.sh
@@ -14,18 +14,18 @@ then
 fi
 
 #Cheking paraments
-[[ "$PREFIX" = "NOT_SET" ]] && { echo "\$PREFIX must be passed as first positional argument"; exit 1; }
-[[ ! $PREFIX =~ ^[1-9]+$ ]] && { echo "\$PREFIX must be digits from 1"; exit 1; }
+[[ "$PREFIX" = "NOT_SET" ]] && { echo "PREFIX must be passed as first positional argument"; exit 1; }
+[[ ! $PREFIX =~ ^(([0-9]+)\.)?[0-9]+$ ]] && { echo "PREFIX must be digits from 1 or like (192.168)"; exit 1; }
 if [[ -z "$INTERFACE" ]]; then
-    echo "\$INTERFACE must be passed as second positional argument"
+    echo "INTERFACE must be passed as second positional argument"
     exit 1
 fi
-[[ ! $SUBNET =~ ^[1-9]+$ ]] && { echo "\$SUBNET must be digits from 0"; exit 1; }
-[[ ! $HOST =~ ^[1-9]+$ ]] && { echo "\$HOST must be digits from 0"; exit 1; }
+[[ ! $SUBNET =~ ^[0-9]+$ ]] && { echo "$SUBNET must be digits from 0"; exit 1; }
+[[ ! $HOST =~ ^[0-9]+$ ]] && { echo "$HOST must be digits from 0"; exit 1; }
 
 #Checking SUBNET and HOST
 if [[ $# -eq 3 ]];then
-	for HOST2 in {1..255}
+	for HOST2 in {0..255}
 	do
 		echo "[*] IP : ${PREFIX}.${SUBNET}.${HOST2}"
 		arping -c 3 -i "$INTERFACE" "${PREFIX}.${SUBNET}.${HOST2}" 2> /dev/null
@@ -34,9 +34,9 @@ elif [[  $# -eq 4 ]];then
 	echo "[*] IP : ${PREFIX}.${SUBNET}.${HOST}"
 		arping -c 3 -i "$INTERFACE" "${PREFIX}.${SUBNET}.${HOST}" 2> /dev/nul
 else
-for SUB in {1..255}
+for SUB in {0..255}
 do
-	for HO in {1..255}
+	for HO in {0..255}
 	do
 		echo "[*] IP : ${PREFIX}.${SUB}.${HO}"
 		arping -c 3 -i "$INTERFACE" "${PREFIX}.${SUB}.${HO}" 2> /dev/null
